56f6f65be3e6d614dd006fd3d24b369578a15a96
Oren Maor <orenmaor@Orens-MacBook-Pro.local>
2020-02-10 18:43:11 -0600

    testing debug output

--- app.py
+++ app.py
@@ -18,20 +18,7 @@ mysql = MySQL(app)
 def index():
     return render_template('index.html')
 
-@app.route('/items/bought',methods=["POST","GET"])
-def bought_items():
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT id,name FROM groups ORDER BY id ASC")
-    groups = list(cur.fetchall())
-    cur.execute("SELECT * FROM location ORDER BY id ASC")
-    locations = list(cur.fetchall())
-
-    form = PurchaseForm()
-    form.group.choices = [(group['id'], group['name']) for group in groups]
-    form.location.choices = [(location['id'], location['long_name']) for location in locations]
-    if not form.validate_on_submit():
-        return render_template('items_purchased.html',form=form)
-    return render_template('items_purchased.html', form=form)
+from items import mod
 
 @app.route("/livesearch",methods=["POST","GET"])
 def livesearch():
