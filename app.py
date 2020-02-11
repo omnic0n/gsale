f5705fcf15236fbb38a9ebce0b49dc644751a8a5
Oren Maor <orenmaor@Orens-MBP.lan>
2020-02-11 17:06:23 -0600

    testing debug output

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ def bought_items():
 @app.route('/items/list')
 def items_list():
     cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM items ORDER BY id ASC")
+    cur.execute("SELECT t1.name, t1.sold, t2.long_name FROM items t1 INNER JOIN platform t2 ON t1.platform = t2.id")
     items = list(cur.fetchall())
     return render_template('items_list.html', items=items)
 
