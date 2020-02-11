f4d0ce27571db96ee581eb7d465b2ecfa5cfbead
Oren Maor <orenmaor@Orens-MacBook-Pro.local>
2020-02-10 19:23:41 -0600

    testing debug output

--- app.py
+++ app.py
@@ -31,13 +31,9 @@ def bought_items():
     form.location.choices = [(location['id'], location['long_name']) for location in locations]
     if request.method == "POST":
         details = request.form
-        print details
         cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO items(group_id, name, description) VALUES (%s, %s, %s)", 
-                    (details['group'], details['name'], details['description'],)) 
-        print str(cur.lastrowid)
-        cur.execute("INSERT INTO purchase(id, location, date, price) VALUES (%s, %s, %s, %s)", 
-                    (str(cur.lastrowid), details['location'], details['date'], details['price'],))
+        cur.execute("INSERT INTO items(group_id, name, description) VALUES (%s, %s, %s)", (details['group'], details['name'], details['description'],)) 
+        cur.execute("INSERT INTO purchase(id, location, date, price) VALUES (%s, %s, %s, %s)", (str(cur.lastrowid), details['location'], details['date'], details['price'],))
         cur.close()
         return redirect(url_for('bought_items'))
     return render_template('items_purchased.html', form=form)
