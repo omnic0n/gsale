5f4d8c3f4ed1d9d3c55bea567ba88358b70a4c12
Oren Maor <orenmaor@Orens-MBP.lan>
2020-02-10 20:28:53 -0600

    testing debug output

--- forms.py
+++ forms.py
@@ -9,7 +9,7 @@ class PurchaseForm(FlaskForm):
     location = SelectField('location', coerce=int)
     group = SelectField('group', coerce=int) 
     price = StringField('price') 
-    date = StringField('date',
+    date = DateField('date',
                            validators=[DataRequired(), Length(min=8, max=8)])
     description = StringField('description')
     submit = SubmitField('Submit')
@@ -18,6 +18,6 @@ class AddGroup(FlaskForm):
     name = StringField('name',
                            validators=[DataRequired(), Length(min=2, max=20)])
     location = SelectField('location', coerce=int)
-    date = StringField('date',
+    date = DateField('date',
                            validators=[DataRequired(), Length(min=8, max=8)])
     submit = SubmitField('Submit')
