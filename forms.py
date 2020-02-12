1efc7b29d821a8e42bfa36ae3679ab10a4481d43
Oren Maor <orenmaor@Orens-MBP.lan>
2020-02-11 20:56:23 -0600

    testing debug output

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, SelectField
+from wtforms import StringField, SubmitField, SelectField, BooleanField
 from wtforms.fields.html5 import DateField
 from wtforms.validators import DataRequired, Length, EqualTo
 
@@ -12,4 +12,16 @@ class PurchaseForm(FlaskForm):
     date = DateField('date',
                            validators=[DataRequired()], format='%Y-%m-%d')
     platform = SelectField('platform', coerce=int)
+    submit = SubmitField('Submit')
+
+class SaleForm(FlaskForm):
+    name = SelectField('name', coerce=int)
+    location = SelectField('location', coerce=int)
+    price = StringField('price')
+    date = DateField('date',
+                           validators=[DataRequired()], format='%Y-%m-%d')
+    tax = BooleanField('taxable')
+    paypal = BooleanField('paypal_used')
+    ebay = BooleanField('ebay_used')
+    shipping_fee = SelectField('shipping_fee', coerce=int)
     submit = SubmitField('Submit')
