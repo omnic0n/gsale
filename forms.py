d6504aca7ce51f6db6cb29f7146ec08400adacbe
Oren Maor <orenmaor@unknown8c8590ca326e.attlocal.net>
2020-02-12 19:08:35 -0600

    testing debug output

--- forms.py
+++ forms.py
@@ -21,7 +21,7 @@ class SaleForm(FlaskForm):
     date = DateField('date',
                            validators=[DataRequired()], format='%Y-%m-%d')
     tax = StringField('tax')
-    paypal = BooleanField('paypal',validators=[DataRequired()], default = 0)
-    ebay = BooleanField('ebay',validators=[DataRequired()], default = 0)
+    paypal = BooleanField('paypal',validators=[DataRequired()], default=False)
+    ebay = BooleanField('ebay',validators=[DataRequired()], default=False)
     shipping_fee = StringField('shipping_fee')
     submit = SubmitField('Submit')
