4b5b909a97036fd504de1cfc7137c39986ab60bd
Oren Maor <orenmaor@unknown8c8590ca326e.attlocal.net>
2020-03-18 21:01:24 -0500

    adding more groups

--- forms.py
+++ forms.py
@@ -17,7 +17,7 @@ class PurchaseForm(FlaskForm):
     name = StringField('name',
                            validators=[DataRequired()])
     location = SelectField('location', coerce=int)
-    group = SelectField('group', coerce=int)
+    group = SelectField('group', coerce=int, default='None')
     price = StringField('price') 
     date = DateField('date',
                            validators=[DataRequired()], format='%Y-%m-%d')
