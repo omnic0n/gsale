6a683267dcd26a68c8ff39e2034d62bb41997eef
Oren Maor <orenmaor@Orens-MBP.attlocal.net>
2020-04-08 00:51:18 -0500

    set localhost

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 app.secret_key = '4T3*%go^Gcn7TrYm'
 
-app.config['MYSQL_HOST'] = 'gsale.cz541jbd6nid.us-east-2.rds.amazonaws.com'
+app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'gsale'
 app.config['MYSQL_PASSWORD'] = 'DR1wZcjTF7858gnu'
 app.config['MYSQL_DB'] = 'gsale'
