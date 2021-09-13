af1942d262ee476a00817fff4125e3d2ecb7d8ac
omnic0n <60856438+omnic0n@users.noreply.github.com>
2021-09-13 16:52:48 -0500

    fixing code

--- sql/table.sql
+++ sql/table.sql
@@ -18,18 +18,13 @@ CREATE TABLE groups (
 
 CREATE TABLE purchase (
      id MEDIUMINT NOT NULL,
-     location MEDIUMINT,
      date DATE,
      price  DECIMAL(6,2),
 );
 
 CREATE TABLE sale (
      id MEDIUMINT NOT NULL,
-     location MEDIUMINT NOT NULL,
      date DATE NOT NULL,
      price  DECIMAL(6,2) NOT NULL,
-     tax DECIMAL(6,2) DEFAULT 0.00,
-     ebay_fee DECIMAL(6,2) DEFAULT 0.00,
-     paypal_fee DECIMAL(6,2) DEFAULT 0.00,
      shipping_fee DECIMAL(5,2) DEFAULT 0.00
 );
