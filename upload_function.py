f267b3e42e594ef759065eef4b00de4406914341
omnic0n <60856438+omnic0n@users.noreply.github.com>
2021-10-04 14:15:32 -0500

    fixing code

--- upload_function.py
+++ upload_function.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, flash, request, redirect, url_for
+from flask import Flask, flash, request, redirect, url_for, send_from_directory
 from werkzeug.utils import secure_filename
 
 UPLOAD_FOLDER = '/home/oren/gsale/gsale/uploads'
@@ -38,6 +38,9 @@ def upload_file():
       <input type=submit value=Upload>
     </form>
     '''           
+@app.route('/uploads/<name>')
+def download_file(name):
+    return send_from_directory(app.config["UPLOAD_FOLDER"], name)
 
 if __name__ == '__main__':
     app.run(debug=True)
