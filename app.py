5682267d69e28c6d16b95764d8aa7af0a6860b47
omnic0n <60856438+omnic0n@users.noreply.github.com>
2022-01-05 19:44:30 -0600

    adding expense report

--- app.py
+++ app.py
@@ -167,7 +167,7 @@ def get_list_of_items_purchased_by_date(date, sold=0):
         cur = mysql.connection.cursor()
         if date:
             print(date)
-             cur.execute("""SELECT 
+            cur.execute("""SELECT 
                         items.id, 
                         items.name, 
                         items.sold,
