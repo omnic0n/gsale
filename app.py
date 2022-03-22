441cadffff0c6e011bef514aca141b5222300430
omnic0n <60856438+omnic0n@users.noreply.github.com>
2022-03-22 15:51:33 -0500

    fixing bad code for get_list_of_items_purchased_date

--- app.py
+++ app.py
@@ -341,7 +341,9 @@ def list_expense():
 def groups_list():
     date = request.args.get('date', type = str)
     groups = get_data.get_all_from_group_and_items(date)
+    print(groups)
     all_groups = get_data.get_all_from_groups(date)
+    print(all_groups)
     return render_template('groups_list.html', groups=groups, all_groups=all_groups)
 
 @app.route('/items/sold_list')
