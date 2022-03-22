49e09b3fb8106653a07c11639cbb2d4b4d9e2764
omnic0n <60856438+omnic0n@users.noreply.github.com>
2022-03-22 16:21:35 -0500

    moved files

---
+++ files.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from upload_function import *
+from werkzeug.utils import secure_filename
+
+import random, os
+
+app = Flask(__name__)
+
+UPLOAD_FOLDER = 'static/uploads'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
+def allowed_file(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def upload_image(file):
+    filename = str(random.getrandbits(128)) + '.jpg'
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(filename)))
+    return filename
