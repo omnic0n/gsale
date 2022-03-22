9676821edd7ff81a46f7cf63a1cb8f590de7d9fd
omnic0n <60856438+omnic0n@users.noreply.github.com>
2022-03-22 16:22:47 -0500

    moved files

--- files.py
+++ files.py
@@ -9,6 +9,9 @@ app = Flask(__name__)
 UPLOAD_FOLDER = 'static/uploads'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
 def allowed_file(filename):
 	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
