f51574f4a3a617ac8d7135cadd64feea27b0e1cc
omnic0n <60856438+omnic0n@users.noreply.github.com>
2022-03-22 21:19:45 -0500

    create default config file

--- config.py
+++ config.py
@@ -1,9 +1,17 @@
 class Config(object):
     DEBUG = False
     TESTING = False
+    SECRET_KEY = ''
+    GOOGLEMAPS_KEY = ''
 
 class ProductionConfig(Config):
-    pass
+
+    MYSQL_HOST = 'localhost'
+    MYSQL_USER = 'gsale'
+    MYSQL_PASSWORD = ''
+    MYSQL_DB = 'gsale'
+    MYSQL_CURSORCLASS = 'DictCursor'
+    MAX_CONTENT_LENGTH = 16 * 1024 * 1024
 
 class DevelopmentConfig(Config):
     DEBUG = True
