f6df54480ae6203828f98f8397a15f5e8cc35e0d
omnic0n <60856438+omnic0n@users.noreply.github.com>
2022-03-22 19:44:05 -0500

    removing extra data

--- set_data.py
+++ set_data.py
@@ -3,18 +3,10 @@ from flask_mysqldb import MySQL
 from datetime import datetime, date, timedelta
 
 import get_data
+
 #Mysql Config
 app = Flask(__name__)
-
 app.secret_key = '4T3*%go^Gcn7TrYm'
-
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'gsale'
-app.config['MYSQL_PASSWORD'] = 'DR1wZcjTF7858gnu'
-app.config['MYSQL_DB'] = 'gsale'
-app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
-
 mysql = MySQL(app)
 
 #Item Data
