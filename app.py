1751118b0bdff63045a06f9dd72aa82787704099
omnic0n <60856438+omnic0n@users.noreply.github.com>
2022-03-30 19:36:34 -0500

    adding timers to items

--- app.py
+++ app.py
@@ -292,7 +292,7 @@ def describe_item():
     if request.method == "POST":
         details = request.form
         set_data.start_timer_packing(details['id'], datetime.now().replace(microsecond=0))
-        return redirect(url_for('sold_items',item=details['id']))
+        return redirect(url_for('sold_items',item=details['id'], timer='true'))
 
     item = get_data.get_data_for_item_describe(id)
     category = get_data.get_category(item[0]['category_id'])
