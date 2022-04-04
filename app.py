02c1bd03c3d2b3eaa2ae8c2622bad12a996daf8e
omnic0n <60856438+omnic0n@users.noreply.github.com>
2022-04-03 22:11:30 -0500

    adding store fees

--- app.py
+++ app.py
@@ -166,6 +166,7 @@ def modify_expense():
     id = request.args.get('id', type = str)
     expense = get_data.get_data_for_expense_describe(id)
     expense_choices = get_data.get_expenses_choices()
+    print(expense)
  
     form = ExpenseForm()
     form.type.choices = [(expense_choice['id'], expense_choice['type']) for expense_choice in expense_choices]
