b26d917bb54e3b73126ea4b1e343776887b712eb
omnic0n <60856438+omnic0n@users.noreply.github.com>
2022-04-03 21:02:47 -0500

    adding store fees

--- forms.py
+++ forms.py
@@ -25,7 +25,7 @@ class ExpenseForm(FlaskForm):
                         default=datetime.today,
                         validators=[DataRequired()], format='%Y-%m-%d')
     id = StringField('id')
-    type = SelectField('type', choices=[(1,"milage"),(2, "item")],coerce=int)
+    type = SelectField('type', choices=[(1,"milage"),(2, "item"),(3, "store")],coerce=int)
     milage = StringField('milage')
     image = FileField('image')
     submit = SubmitField('Submit')
@@ -77,7 +77,7 @@ class ReportsForm(FlaskForm):
     year = SelectField('Year', choices=[
         (2021), (2022), (2023)
     ])
-    expense_type = SelectField('type', choices=[(1,"milage"),(2, "item")],coerce=int)
+    expense_type = SelectField('type', choices=[(1,"milage"),(2, "item"),(3, "store")],coerce=int)
     category = SelectField('category', coerce=int)
     submit = SubmitField('Submit')
 
