71882b6424199380ea951a32470e704450e541c0
Oren Maor <orenmaor@gmail.com>
2022-05-06 04:39:09 -0500

    adding upload function pyc file

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 class Config(object):
     DEBUG = False
     TESTING = False
-    SECRET_KEY = ''
-    GOOGLEMAPS_KEY = ''
+    SECRET_KEY = '4T3*%go^Gcn7TrYm'
+    GOOGLEMAPS_KEY = 'AIzaSyCx0uvY8tyZe7gfOKjEZIgJL__ijm0ojZ0'
 
 class ProductionConfig(Config):
 
     MYSQL_HOST = 'localhost'
     MYSQL_USER = 'gsale'
-    MYSQL_PASSWORD = ''
+    MYSQL_PASSWORD = 'DR1wZcjTF7858gnu'  
     MYSQL_DB = 'gsale'
     MYSQL_CURSORCLASS = 'DictCursor'
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024
