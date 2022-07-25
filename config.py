0c0e3de5d722848b0d6f210e9d1782c282a29603
omnic0n <60856438+omnic0n@users.noreply.github.com>
2022-07-25 13:25:03 -0500

    removing default values

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 class Config(object):
     DEBUG = False
     TESTING = False
-    SECRET_KEY = '4T3*%go^Gcn7TrYm'
-    GOOGLEMAPS_KEY = 'AIzaSyCx0uvY8tyZe7gfOKjEZIgJL__ijm0ojZ0'
+    SECRET_KEY = 'xxx'
+    GOOGLEMAPS_KEY = 'xxx'
 
 class ProductionConfig(Config):
 
     MYSQL_HOST = 'localhost'
     MYSQL_USER = 'gsale'
-    MYSQL_PASSWORD = 'DR1wZcjTF7858gnu'  
+    MYSQL_PASSWORD = 'xxx'  
     MYSQL_DB = 'gsale'
     MYSQL_CURSORCLASS = 'DictCursor'
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024
