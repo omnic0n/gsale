9c916f42b8fa68e734da1bc1ad85090882fec49e
omnic0n <60856438+omnic0n@users.noreply.github.com>
2023-03-02 17:01:11 -0600

    adding login

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def login():
         username = request.form['username']
         password = request.form['password']
         # Check if account exists using MySQL
-        msg = get_data.login_data(username=username, password=password)
+        msg = function.login_data(username=username, password=password)
         if 'loggedin' in session:
             return redirect(url_for('index'))    
     return render_template('login.html', msg=msg)
