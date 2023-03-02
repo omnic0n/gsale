00b4e070117ae312e51085c88d6c306e833605a6
omnic0n <60856438+omnic0n@users.noreply.github.com>
2023-03-02 15:10:47 -0600

    adding login

--- app.py
+++ app.py
@@ -56,24 +56,18 @@ def index():
     profit = get_data.get_profit()
     return render_template('index.html', profit=profit)
 
-@app.route('/reports/<name>',methods=["GET", "POST"])
-def reports(name):
-    print(str(name))
-    if str(name) == "profit":
-        print("Matched")
-        def reports_profit():
-            form = ReportsForm()
+@app.route('/reports/profit',methods=["GET", "POST"])
+def reports_profit():
+    form = ReportsForm()
 
-            if request.method == "POST":
-                details = request.form
-                start_date, end_date = function.set_dates(details)
-                sold_dates = get_data.get_group_sold_from_date(start_date, end_date)
-                purchased_dates = get_data.get_purchased_from_date(start_date, end_date)
-                expenses= get_data.get_all_from_expenses_date(start_date, end_date)
-                return render_template('reports_profit.html', form=form, sold_dates=sold_dates, purchased_dates=purchased_dates, expenses=expenses)
-            return render_template('reports_profit.html', form=form)
-
-    return redirect(url_for('index'))    
+    if request.method == "POST":
+        details = request.form
+        start_date, end_date = function.set_dates(details)
+        sold_dates = get_data.get_group_sold_from_date(start_date, end_date)
+        purchased_dates = get_data.get_purchased_from_date(start_date, end_date)
+        expenses= get_data.get_all_from_expenses_date(start_date, end_date)
+        return render_template('reports_profit.html', form=form, sold_dates=sold_dates, purchased_dates=purchased_dates, expenses=expenses)
+    return render_template('reports_profit.html', form=form)
 
 
 @app.route('/reports/sales',methods=["GET", "POST"])
