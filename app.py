d05619021d3b8ff931fd9fb2706ee7fa31f684ae
omnic0n <60856438+omnic0n@users.noreply.github.com>
2023-03-02 14:24:02 -0600

    adding login

--- app.py
+++ app.py
@@ -51,15 +51,14 @@ def logout():
    # Redirect to login page
    return redirect(url_for('login'))
 
-def login_check():
-    if not 'loggedin' in session:
-        return redirect(url_for('login'))
-
 @app.route('/')
 def index():
-    login_check()
     profit = get_data.get_profit()
     return render_template('index.html', profit=profit)
+
+@app.route('/reports/<name>',methods=["GET", "POST"])
+def reports(name):
+    print(name)
 
 @app.route('/reports/profit',methods=["GET", "POST"])
 def reports_profit():
