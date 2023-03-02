97af3ae8d2b01d5a57ce2054dc40a8b759497081
omnic0n <60856438+omnic0n@users.noreply.github.com>
2023-03-02 11:52:18 -0600

    adding login

--- app.py
+++ app.py
@@ -27,7 +27,28 @@ MySQL(app)
 def login():
     # Output message if something goes wrong...
     msg = ''
-    return render_template('login.html', msg='')
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        # Create variables for easy access
+        username = request.form['username']
+        password = request.form['password']
+        # Check if account exists using MySQL
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE username = %s AND password = %s', (username, password,))
+        # Fetch one record and return result
+        account = cursor.fetchone()
+        # If account exists in accounts table in out database
+        if account:
+            # Create session data, we can access this data in other routes
+            session['loggedin'] = True
+            session['id'] = account['id']
+            session['username'] = account['username']
+            # Redirect to home page
+            return 'Logged in successfully!'
+        else:
+            # Account doesnt exist or username/password incorrect
+            msg = 'Incorrect username/password!'
+    # Show the login form with message (if any)
+    return render_template('login.html', msg=msg)
 
 @app.route('/')
 def index():
