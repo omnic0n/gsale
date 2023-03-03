6101d7629340f1f1b47010abf54974e72640ae8e
omnic0n <60856438+omnic0n@users.noreply.github.com>
2023-03-02 20:43:31 -0600

    adding login

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def login():
     # Output message if something goes wrong...
     msg = ''
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
-        ip = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)   
+        ip = request.environ['REMOTE_ADDR']  
         # Create variables for easy access
         username = request.form['username']
         password = request.form['password']
