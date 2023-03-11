e02f0653eb744fa8950e857bc365e29527042dda
omnic0n <60856438+omnic0n@users.noreply.github.com>
2023-03-10 22:07:42 -0600

    adding cases

--- get_data.py
+++ get_data.py
@@ -357,7 +357,7 @@ def get_all_from_cases(platform):
     cur.execute("""
                 SELECT 
                 cases.name,
-                case.id,
+                cases.id,
                 cases.platform as platform_id,
                 platform.name as platform_name
                 FROM cases
