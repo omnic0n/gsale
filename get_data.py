f8b49645834cba57532118cbdd19fab8704400ee
omnic0n <60856438+omnic0n@users.noreply.github.com>
2023-03-10 22:19:05 -0600

    adding cases

--- get_data.py
+++ get_data.py
@@ -378,5 +378,4 @@ def get_data_for_case_describe(case_id):
                     from cases
                     INNER JOIN platform platform on cases.platform = platform.id 
                     WHERE cases.id = %s""", (case_id, ))
-    print(list(cur.fetchall()))
     return list(cur.fetchall())
