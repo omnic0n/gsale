54a18527174f45d4ae2672866e3a76c502c355bd
omnic0n <60856438+omnic0n@users.noreply.github.com>
2023-03-24 09:49:58 -0500

    adding date to collection

--- get_data.py
+++ get_data.py
@@ -44,7 +44,7 @@ def get_all_from_group_and_items(date):
 def get_all_from_groups(date):
     cur = mysql.connection.cursor()
     if not date:
-        date = datetime.date.today().year
+        date = str(datetime.date.today().year)
     cur.execute("SELECT * FROM collection WHERE date LIKE %s AND collection.account = %s ORDER BY name ASC", ('%' + date + '%', session['id'], ))
     return list(cur.fetchall())
 
