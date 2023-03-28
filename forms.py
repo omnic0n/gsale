e702fc9cd70c792a806fc5bb515c13cc79c21ca4
omnic0n <60856438+omnic0n@users.noreply.github.com>
2023-03-28 12:37:01 -0500

    modify describe group

--- forms.py
+++ forms.py
@@ -79,7 +79,7 @@ class ReportsForm(FlaskForm):
     month = SelectField('Month', choices=[
         (1,"January"),(2, "February"),(3, "March"),(4, "April"),(5, "May"),(6, "June"),(7, "July"),(8, "August"),(9, "September"),(10, "October"),(11, "November"),(12, "December")
     ])
-    year = SelectField('Year', choices=[
+    year = SelectField('Year', default=datetime.now().year choices=[
         (2021), (2022), (2023)
     ])
     expense_type = SelectField('type', coerce=int)
