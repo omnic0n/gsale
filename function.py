73f88de4e77c2c085b4d56574e1d173557ebf072
omnic0n <60856438+omnic0n@users.noreply.github.com>
2023-03-28 13:52:09 -0500

    adding total items to group list

--- function.py
+++ function.py
@@ -17,10 +17,10 @@ def set_dates(details):
     year = int(details['year'])
     month = int(details['month'])
     date = details['date']
-    if(details['type'] == "Year"):
+    if(details['type'] == "2"):
         start_date = ("%s-01-01") % (year)
         end_date = datetime.strptime(("%s-01-01") % (year + 1), '%Y-%m-%d').date() - timedelta(days=1)
-    elif(details['type'] == "Month"):
+    elif(details['type'] == "3"):
         start_date = ("%s-%s-01") % (year, month)
         end_date = datetime.strptime(("%s-%s-01") % (year,month),'%Y-%m-%d').date() + relativedelta(months=1) - timedelta(days=1)
     else:
