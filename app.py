9090470cc542ac8e2ddca27bad314128cbac1f2a
omnic0n <60856438+omnic0n@users.noreply.github.com>
2023-05-17 17:03:36 -0500

    adding day

--- app.py
+++ app.py
@@ -310,7 +310,7 @@ def bought_items():
 
     form.category.choices = [(category['id'], category['type']) for category in categories]
 
-    form.list_date.data = get_data.get_all_from_group(group_id)['date']
+    form.list_date.data = get_data.get_all_from_group(group_id)
 
     if request.method == "POST":
         details = request.form
