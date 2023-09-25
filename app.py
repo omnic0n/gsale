3d863eb73981c57fdc310889c668d379b1530d84
oren <orenmaor@gmail.com>
2023-09-24 21:48:31 -0500

    adding sold_items to groups

--- app.py
+++ app.py
@@ -450,6 +450,7 @@ def list_items():
     if storage is None:
         storage = "%"
 
+    print(session['id'])
     items = get_data.get_list_of_items_purchased_by_date(sold_date, purchase_date, sold, list_date, storage)
     print(items)
 
