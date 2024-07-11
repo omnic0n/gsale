905e81a5f4b4f1bb977d433319e8a76f8a63fca4
oren <orenmaor@gmail.com>
2024-07-11 15:43:54 -0500

    adding day

--- forms.py
+++ forms.py
@@ -88,7 +88,7 @@ class ReportsForm(FlaskForm):
         (1,"January"),(2, "February"),(3, "March"),(4, "April"),(5, "May"),(6, "June"),(7, "July"),(8, "August"),(9, "September"),(10, "October"),(11, "November"),(12, "December")
     ])
     year = SelectField('Year', default=datetime.now().year, choices=[
-        ('%','All'), (2021), (2022), (2023), (2024)
+        (0,'All'), (2021), (2022), (2023), (2024)
     ])
     day = SelectField('Day', choices=[
         (1,"Sunday"),(2, "Monday"),(3, "Tuesday"),(4, "Wednesday"),(5, "Thursday"),(6, "Friday"),(7, "Saturday")
