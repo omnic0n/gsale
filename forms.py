05f9952962a67a770e1a3c9e9c22e88a7d6f7313
oren <orenmaor@gmail.com>
2025-01-28 11:24:47 -0600

    adding all

--- forms.py
+++ forms.py
@@ -15,7 +15,7 @@ class GroupForm(FlaskForm):
     image = FileField('image')
 
     listYear = SelectField('Year', default=datetime.now().year, choices=[
-        (2021), (2022), (2023), (2024), (2025)
+        (2021), (2022), (2023), (2024), (2025), ("All")
     ])
     submit = SubmitField('Submit')
 
