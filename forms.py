850f8e5109517e31926644440c08cdf42b27b613
oren <orenmaor@gmail.com>
2025-12-30 09:49:01 -0600

    adding 2026

--- forms.py
+++ forms.py
@@ -19,7 +19,7 @@ class GroupForm(FlaskForm):
     location_address = StringField('location_address')
 
     listYear = SelectField('Year', default=datetime.now().year, choices=[
-        (2021), (2022), (2023), (2024), (2025), ("All")
+        (2021), (2022), (2023), (2024), (2025), (2026), ("All")
     ])
     submit = SubmitField('Submit')
 
@@ -86,7 +86,7 @@ class ReportsForm(FlaskForm):
         (1,"January"),(2, "February"),(3, "March"),(4, "April"),(5, "May"),(6, "June"),(7, "July"),(8, "August"),(9, "September"),(10, "October"),(11, "November"),(12, "December")
     ])
     year = SelectField('Year', default=datetime.now().year, choices=[
-        (2021), (2022), (2023), (2024), (2025), ("All")
+        (2021), (2022), (2023), (2024), (2025), (2026), ("All")
     ])
     day = SelectField('Day', choices=[
         (1,"Sunday"),(2, "Monday"),(3, "Tuesday"),(4, "Wednesday"),(5, "Thursday"),(6, "Friday"),(7, "Saturday")
